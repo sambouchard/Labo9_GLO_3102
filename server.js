882ba7e490d106028da4fc7c30fbd93ec76a4ecf
ULAVAL\SABOU350 <samuel.bouchard.7@ulaval.ca>
2018-03-29 22:12:51 -0400

    Got to indlude socket.io-client in html file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const path = require('path');
-const nJwt = require('njwt');
+let app = express();
+const http = require('http')
+let server = http.createServer(app)
+const io = require('socket.io')(server);
 const cors = require('cors');
-const Cookies = require('js-cookie');
-const uuidv4 = require('uuid/v4');
 const corsOptions = {
     origin: '*',
     methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'UPDATE'],
@@ -14,64 +14,24 @@ const port = process.env.PORT || 8080;
 
 
 
-const users = [{username: 'sam', password: '1234'}, {username:'vaun' , password: 'orage'}];
-const secretkey = uuidv4();
+const users = [];
 
-const app = express();
-const bodyParser = require('body-parser');
-
-
-app.use(bodyParser.json());
-app.use(cors(corsOptions));
-app.use(function(req,res,next){
-    let _send = res.send;
-    let sent = false;
-    res.send = function(data){
-        if(sent) return;
-        _send.bind(res)(data);
-        sent = true;
-    };
-    next();
+io.on('connection',function(socket){
+    console.log('A user connected');
+    socket.on('chatmessage',function (msg) {
+        console.log('message is' + msg)
+    });
 });
 
+server.listen(3000);
 
-app.post('/login',function (req,res) {
-    let usertoidentify = req.body.username;
-    let passtoidentify = req.body.password;
-    for(let i = 0; i < users.length; i++){
-        if(usertoidentify == users[i].username && passtoidentify == users[i].password){
-            let claims = {
-                sub: usertoidentify,
-                iss: 'labo8/server',
-                permissions: 'access user profile'
-            }
-            let jwt = nJwt.create(claims,secretkey);
-            let token = jwt.compact();
-            Cookies.set('access_token',token,{path: '/userprofile'});
-            res.cookie('access_token',token);
-        }
-        else {
-            res.status(403).send('Combinaison invalide');
-        }
-    }
-});
-
-app.get('/userprofile',function (req,res) {
-    let cookies = Cookies.get('access_token');
-    if(nJwt.verify(cookies,secretkey)){
-        res.render('./userprofile.html')
-    }
-    else{
-        res.render('./login.html')
-    }
-
-})
-
-app.post('/users',function (req,res) {
+app.post('/postusername',function (req,res) {
     let username = req.body.username;
-    let password = req.body.password;
-    users.append({username:username,password:password})
-})
+    let id = req.body.id;
+    users.append({username:username,id: id})
+});
+
+
 
 app.listen(port, function() {
     console.log('Server listening.')
