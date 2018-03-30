dc48ece3ddabc9837c4cd878e7d3441d55a35a25
ULAVAL\SABOU350 <samuel.bouchard.7@ulaval.ca>
2018-03-30 14:40:16 -0400

    Finalized

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-const express = require('express');
+let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
-const http = require('http')
-let server = http.createServer(app)
-const io = require('socket.io')(server);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+http.listen(8080, function(){
+    console.log('listening on *:3000');
+})
 const cors = require('cors');
 const corsOptions = {
     origin: '*',
     methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'UPDATE'],
     credentials: true
 };
+
+app.use(bodyParser());
 
 const port = process.env.PORT || 8080;
 
@@ -18,24 +23,21 @@ const users = [];
 
 io.on('connection',function(socket){
     console.log('A user connected');
-    socket.on('chatmessage',function (msg) {
-        console.log('message is' + msg)
-    });
+    socket.on('chatmessage',function (msg, username) {
+        io.emit('chatmessage',msg , username)
+        });
 });
 
-server.listen(3000);
-
-app.post('/postusername',function (req,res) {
-    let username = req.body.username;
-    let id = req.body.id;
-    users.append({username:username,id: id})
+app.get('/',function (req,res) {
+    res.sendFile(__dirname+'/userinterface.html');
 });
 
 
 
-app.listen(port, function() {
-    console.log('Server listening.')
-});
+
+
+
+
 
 
 
